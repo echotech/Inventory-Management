19c788aebf95b5426ba7e152cdde550a4a7617b8
jreis <jed.reisner@gmail.com>
2017-02-22 15:25:43 -0700

    Formatting and added missing methods.

--- src/wgu/model/Part.java
+++ src/wgu/model/Part.java
@@ -18,28 +18,28 @@ public abstract class Part{
     private SimpleIntegerProperty partID;
 
     //Setters and Getters
+    public void setName(String name){this.name.set(name);}
+    public String getName(){return name.get();}
+    public SimpleStringProperty nameProperty(){return name;}
+
+    public void setPrice(Double price){this.price.set(price);}
+    public Double getPrice(){return price.get();}
+    public SimpleDoubleProperty priceProperty(){return price;}
+
     public void setInstock(int amt){this.instock.set(amt);}
-
     public int getInstock(){return instock.get();}
-
     public SimpleIntegerProperty inStockProperty(){return instock;}
 
     public void setMin(int min){this.min.set(min);}
-
     public int getMin(){return min.get();}
-
     public SimpleIntegerProperty minProperty(){return min;}
 
     public void setMax(int max){this.max.set(max);}
-
     public int getMax(){return max.get();}
-
     public SimpleIntegerProperty maxProperty(){return max;}
 
     public void setPartID(int id){this.partID.set(next_id.incrementAndGet());  }
-
     public int getPartID(){return partID.get();}
-
     public SimpleIntegerProperty partIDProperty(){return partID;}
 
 
