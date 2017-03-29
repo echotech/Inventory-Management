a193a896949af205ab42cc5b4a7899d0a3037812
Jed <jed.reisner@gmail.com>
2017-03-28 22:39:58 -0600

    Added TODOS

---
+++ src/wgu/Exceptions.java
@@ -0,0 +1,19 @@
+package wgu;
+
+/**
+ * Created by admin on 3/28/2017.
+ */
+public class Exceptions {
+    //TODO add exception set 1
+    //TODO throw exception when inventory value exceeds min or max for part or product
+    //TODO throw exception preventing min field from being higher than max
+    //TODO throw exception preventing max from having value below min
+    //TODO ensure products have at least one part
+
+
+    //TODO set 2
+    //TODO preventing user from deleting product that has parts
+    //TODO include a confirm dialoge for Delete and Cancel buttons
+    //TODO ensure that price of product can't be less than cost of parts
+    //TODO ensure that product must have a name, price, category and inventory level
+}
